35a42d9d394504d347322a731b402f3164b3d169
kenmutesh <kenmutesh901@gmail.com>
2022-06-30 15:30:57 -0700

    ken

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-nclude <stdio.h>
-
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-int main(void)
-{
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
-}
+#include<stdio.h> 
+ /** 
+  *   * main - Entry point 
+  *    * 
+  *     * Return: Alwys 0 (success) 
+  *      */ 
+ int main(void) 
+	 { 
+		          puts("\"Programming is like building a multilingual puzzle"); 
+		           return (0); 
+		    }

969fc17b4945ba84057476b5e183ad5d6125cbb6
kenmutesh <kenmutesh901@gmail.com>
2022-06-30 14:50:11 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 /**
@@ -8,6 +7,6 @@
  *     */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

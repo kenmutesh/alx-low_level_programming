b9f969685dbc482e058921f924cf512061bc831e
kenmutesh <kenmutesh901@gmail.com>
2022-06-30 16:15:37 -0700

    ken1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- *  * main - to print the output
- *   * Return: 0 shows success non zero shows error
- *    */
+ * main - to print the output
+ * Return: 0 shows success non zero shows error
+ */
 
 int main(void)
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }

99f5b2cb55e68b44c116c00bd9d4c2727de4800c
kenmutesh <kenmutesh901@gmail.com>
2022-06-30 16:32:17 -0700

    ken

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,6 +9,7 @@ int main(void)
 	char c;
 	int i;
 	float f;
+
 	printf("Size of a char: %lu byte\(s\)\n", sizeof(c));
 	printf("Size of an int: %lu byte\(s\)\n", sizeof(i));
 	printf("Size of a long int: %lu byte\(s\)\n", sizeof(long int));
